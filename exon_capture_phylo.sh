c7fcc2cd8439eeb7462ff905b7d8484a6002ff53
Ben Bai <u5205339@anu.edu.au>
2014-01-20 17:03:31 +1100

    [FIX] Adds mutexes and auto-detect of SGEJobArrays
    Fixes #8. When using SGE Job Arrays to run samples in parallel, only 1
    sample is allowed to prepare data, and only the last sample to complete
    is allowed to gather data.
    - Currently using mkdir-based locking:
          http://mywiki.wooledge.org/BashFAQ/045

--- exon_capture_phylo.sh
+++ exon_capture_phylo.sh
@@ -6,10 +6,9 @@
 #$ -N job_exon_capture_phylo
 #$ -R y
 #$ -l virtual_free=10G,h_vmem=12G
-# -q bigmem.q
-# -pe threads 5
-# -t 1-1
-# -tc 20 
+#$ -q bigmem.q
+#$ -tc 20 
+#$ -t 1-2
 
 # Mail on job abortion/end
 # Rerun on abort
@@ -50,23 +49,86 @@ fi
 
 cd "$OUT_DIR" || exit 
 
-# prepare data
-"$SCRIPT_DIR/sh/prepare_data.sh" "$SCRIPT_DIR/$CONFIG_FILE"
+# Check if we not using a job array
+if [ -z "$SGE_TASK_ID" ] || [ "$SGE_TASK_ID" == "undefined" ]; then
 
-# Check if we are using a job array
-# if [ -n $SGE_TASK_ID ]; then
-# libs=( $(cat $LIBRARIES_LIST) )
-# filnum=$[SGE_TASK_ID-1]
-# parfil=${libs[$filnum]}
+    echo Using xargs to run $MAX_PARALLEL_SAMPLES samples in parallel 
 
-# assemble contigs
-< "$LIBRARIES_LIST" xargs -n 1 --max-procs 20 -I {} "$SCRIPT_DIR/sh/assemble_exons.sh" {} "$SCRIPT_DIR/$CONFIG_FILE"
+    # prepare data
+    "$SCRIPT_DIR/sh/prepare_data.sh" "$SCRIPT_DIR/$CONFIG_FILE"
 
-# call variants
-< "$LIBRARIES_LIST" xargs -n 1 --max-procs 20 -I {} "$SCRIPT_DIR/sh/call_variants.sh" {} "$SCRIPT_DIR/$CONFIG_FILE"
+    # assemble contigs
+    < "$LIBRARIES_LIST" xargs -n 1 --max-procs "$MAX_PARALLEL_SAMPLES" -I {} "$SCRIPT_DIR/sh/assemble_exons.sh" "{}" "$SCRIPT_DIR/$CONFIG_FILE"
 
-# gather contigs by exon and by sample
-"$SCRIPT_DIR/sh/gather_exons.sh" "$SCRIPT_DIR/$CONFIG_FILE"
+    # call variants
+    < "$LIBRARIES_LIST" xargs -n 1 --max-procs "$MAX_PARALLEL_SAMPLES" -I {} "$SCRIPT_DIR/sh/call_variants.sh" "{}" "$SCRIPT_DIR/$CONFIG_FILE"
+
+    # gather contigs by exon and by sample
+    "$SCRIPT_DIR/sh/gather_exons.sh" "$SCRIPT_DIR/$CONFIG_FILE"
+
+# We are using a job array
+else
+
+    readarray -t SAMPLE_NAMES_ARRAY < "$LIBRARIES_LIST"
+    sample_num=${#SAMPLE_NAMES_ARRAY[@]}
+    sample_name=${SAMPLE_NAMES_ARRAY[$SGE_TASK_ID - 1]}
+    echo SGE Job Array SGE_TASK_ID=$SGE_TASK_ID detected. Using sample $sample_name out of $sample_num samples
+
+    # data is already prepared continue
+    if [ -d "$OUT_DIR/prepare_data_complete.lock" ]; then
+        echo already prepped!
+        :
+    # else check if the data is being prepared. If not, prepare the data, then inform that the data is prepared
+    elif mkdir "$OUT_DIR/preparing_data.lock"; then
+        echo I will prep
+        "$SCRIPT_DIR/sh/prepare_data.sh" "$SCRIPT_DIR/$CONFIG_FILE"
+        mv "$OUT_DIR/preparing_data.lock/" "$OUT_DIR/prepare_data_complete.lock/"
+
+    # else poll until prepare_data is complete
+    else
+        
+        echo I will wait
+        while ! [ -d "$OUT_DIR/prepare_data_complete.lock/" ]; do
+            sleep 10
+            echo waiting for prepare data complete
+        done
+
+    fi
+    
+    echo main part now
+
+    # assemble contigs
+    "$SCRIPT_DIR/sh/assemble_exons.sh" "$sample_name" "$SCRIPT_DIR/$CONFIG_FILE"
+
+    # call variants
+    "$SCRIPT_DIR/sh/call_variants.sh" "$sample_name" "$SCRIPT_DIR/$CONFIG_FILE"
+
+    # inform that the sample is done
+    echo I finished main part
+    echo "$SGE_TASK_ID" >> "$OUT_DIR/completed_ids.txt"
+
+    # if all samples are done, this job needs to gather
+    if [ "$(wc -l < "$OUT_DIR/completed_ids.txt")" -eq "$sample_num" ]; then
+
+        # lock it to make sure this is the only job gathering
+        if mkdir "$OUT_DIR/gather_data.lock"; then
+            echo I gather!
+            # gather data 
+            "$SCRIPT_DIR/sh/gather_exons.sh" "$SCRIPT_DIR/$CONFIG_FILE"
+            
+            # clean up
+            rmdir "$OUT_DIR/prepare_data_complete.lock/"
+            rm "$OUT_DIR/completed_ids.txt"
+            rmdir "$OUT_DIR/gather_data.lock"
+        else
+            echo race condition avoided. heehee
+        fi
+
+    else
+        echo I am not the last job. cya, not gathering
+    fi
+
+fi
 
 echo ===== Finished at $(date) =====
 exit
